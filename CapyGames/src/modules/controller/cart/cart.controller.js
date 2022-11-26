84e61a08a38db67709575c1821b0493b72c4e19d
BetjaderOD <20213tn032@utez.edu.mx>
2022-11-25 22:26:21 -0600

    carrito completo

--- CapyGames/src/modules/controller/cart/cart.controller.js
+++ CapyGames/src/modules/controller/cart/cart.controller.js
@@ -1,6 +1,6 @@
 const { Response, Router } = require('express');
 const { validateError } = require ("../../../utils/functions");
-const { findAll, findById, save} = require("./cart.gateway");
+const { findAll, findById, save, update, remove} = require("./cart.gateway");
 
 
 const getAll = async (req, res = Response) => {
@@ -42,11 +42,40 @@ const insert = async (req, res = Response) => {
         res.status(400).json({ message });
     }
 }
+const updateCart = async (req, res = Response) => {
+    try {
+        const { cart_id,game_id,costumers_id,cart_quantity } = req.body;
+        const cart = await update({
+            cart_id,
+            game_id,
+            costumers_id,
+            cart_quantity
+        });
+        res.status(200).json(cart);
+    }catch(error) {
+        console.log(error);
+        const message = validateError(error);
+        res.status(400).json({ message });
+    }
+}
+const deleteCart = async (req, res = Response) => {
+    try {
+        const { id } = req.params;
+        const cart = await remove(id);
+        res.status(200).json(cart);
+    }catch(error) {
+        console.log(error);
+        const message = validateError(error);
+        res.status(400).json({ message });
+    }
+}
 
 const cartRouter = Router();
 cartRouter.get("/", [], getAll);
 cartRouter.get("/:id", [], getById);
 cartRouter.post("/", [], insert);
+cartRouter.put("/", [], updateCart);
+cartRouter.delete("/:id", [], deleteCart);
 
 module.exports = {
     cartRouter,
--- CapyGames/src/modules/controller/cart/cart.gateway.js
+++ CapyGames/src/modules/controller/cart/cart.gateway.js
@@ -1,12 +1,12 @@
 const { query } = require ("../../../utils/mysql");
 const findAll = async () => {
-    const sql = "select games.game_name, customers.customer_name, cart.cart_id from cart, games, customers where cart.game_id = games.game_id and cart.customer_id = customers.customer_id;";
+    const sql = "select games.game_name, customers.customer_name, cart.cart_id , cart.cart_quantity from cart, games, customers where cart.game_id = games.game_id and cart.customer_id = customers.customer_id;";
     return await query(sql, []);
 };
 const findById = async (id) => {
     if (Number.isNaN(id)) throw Error('Wrong type');
     if (!id) throw Error('Missing fields');
-    const sql = "select games.game_name, customers.customer_name, cart.cart_id from cart, games, customers where cart.game_id = games.game_id and cart.customer_id = customers.customer_id and cart.cart_id = ?;";
+    const sql = "select games.game_name, customers.customer_name, cart.cart_id , cart.cart_quantity from cart, games, customers where cart.game_id = games.game_id and cart.customer_id = customers.customer_id and cart.cart_id = ?;";
     return await query(sql, [id]);
 }
 const save = async (cart) => {
@@ -25,8 +25,34 @@ const save = async (cart) => {
     ]);
     return { ...cart, id: insertedId };
 }
+const update = async (cart) => {
+    if (
+        !cart.game_id ||
+        !cart.costumers_id||
+        !cart.cart_quantity
+
+
+    )
+        throw Error('Missing fields');
+    const sql = "UPDATE cart SET game_id = ?, customer_id = ?, cart_quantity = ? WHERE cart_id = ?;";
+    const { insertedId } = await query(sql, [
+        cart.game_id,
+        cart.costumers_id,
+        cart.cart_quantity,
+        cart.cart_id
+    ]);
+    return { ...cart, id: insertedId };
+}
+const remove = async (id) => {
+    if (Number.isNaN(id)) throw Error('Wrong type');
+    if (!id) throw Error('Missing fields');
+    const sql = "DELETE FROM cart WHERE cart_id = ?;";
+    return await query(sql, [id]);
+}
 module.exports = {
     findAll,
     findById,
     save,
+    update,
+    remove
 }

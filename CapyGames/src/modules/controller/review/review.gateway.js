7c287d8ae9a26b6354e8ae1fb3972ded91b45b61
Abraham <20213tn037@utez.edu.mx>
2022-12-15 21:59:44 -0600

    Review funcional

--- CapyGames/src/modules/controller/review/review.gateway.js
+++ CapyGames/src/modules/controller/review/review.gateway.js
@@ -8,23 +8,40 @@ const findAll = async () => {
 const findById = async (id) => {
   if (Number.isNaN(id)) throw Error("Wrong type");
   if (!id) throw Error("Missing fields");
-  const sql = `SELECT * FROM reviews WHERE review_id = ?`;
+  const sql = `SELECT * FROM reviews WHERE game_id = ?`;
   return await query(sql, [id]);
 };
 
+// const save = async (review) => {
+//   if (
+//     !review.review_date ||
+//     !review.review_title ||
+//     !review.review_description ||
+//     !review.review_rating
+//   )
+//     throw Error("Missing fields");
+//   const sql = `INSERT INTO reviews (review_date, review_title, review_description, review_rating) VALUES (?, ?, ?, ?)`;
+//   const { insertedId } = await query(sql, [
+//     review.review_date,
+//     review.review_title,
+//     review.review_description,
+//     review.review_rating,
+//   ]);
+//   return { ...review, id: insertedId };
+// };
+
+//save
 const save = async (review) => {
   if (
-    !review.customer_id ||
-    !review.game_id ||
-    !review.review_date ||
-    !review.review_title ||
-    !review.review_description ||
-    !review.review_rating
-  )
-    throw Error("Missing fields");
-  const sql = `INSERT INTO reviews (customer_id, game_id, review_date, review_title, review_description, review_rating) VALUES (?, ?, ?, ?, ?, ?)`;
+    !review.game_id||
+  !review.review_date||
+  !review.review_title||
+  !review.review_description ||
+  !review.review_rating
+)
+  throw Error("Missing fields");
+  const sql = "INSERT INTO reviews (game_id, review_date, review_title, review_description, review_rating) VALUES (?, ?, ?, ?, ?)";
   const { insertedId } = await query(sql, [
-    review.customer_id,
     review.game_id,
     review.review_date,
     review.review_title,
@@ -37,7 +54,6 @@ const save = async (review) => {
 const update = async (review) => {
   if (
     !review.review_id ||
-    !review.customer_id ||
     !review.game_id ||
     !review.review_date ||
     !review.review_title ||
@@ -45,9 +61,8 @@ const update = async (review) => {
     !review.review_rating
   )
     throw Error("Missing fields");
-  const sql = `UPDATE reviews SET customer_id = ?, game_id = ?, review_date = ?, review_title = ?, review_description = ?, review_rating = ? WHERE review_id = ?`;
+  const sql = `UPDATE reviews SET game_id = ?, review_date = ?, review_title = ?, review_description = ?, review_rating = ? WHERE review_id = ?`;
   const { insertedId } = await query(sql, [
-    review.customer_id,
     review.game_id,
     review.review_date,
     review.review_title,
